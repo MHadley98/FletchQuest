c1fdba14012e2f733499de6414f0abfd16bca0a5
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2019-05-11 17:42:07 +0100

    Added walkable ground in L1 and main menu screen

--- FletchQuest/Assets/Scripts/Fletcher.cs
+++ FletchQuest/Assets/Scripts/Fletcher.cs
@@ -4,13 +4,69 @@ using UnityEngine;
 
 public class Fletcher : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    //designer variables
+    public float speed = 10;
+    public float jumpSpeed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+    public string jumpButton = "Jump";
+
+    public Animator playerAnimator;
+    public SpriteRenderer playerSprite;
+    public Collider2D playerCollider;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+        //Get axis input from Unity
+        float leftRight = Input.GetAxis(horizontalAxis);
+
+        //Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, 0);
+
+        //Make direction vector length 1
+        direction = direction.normalized;
+
+        //calculate velocity
+        Vector2 velocity = direction * speed;
+        velocity.y = physicsBody.velocity.y;
+
+        //give the velocity to the rigid body
+        physicsBody.velocity = velocity;
+
+        //Jumpuing
+
+        //detect if touching ground
+        //Get the LayerMask from Unity using the name of the layer
+        LayerMask groundLayerMask = LayerMask.GetMask("Ground");
+        //Ask the player's collider if we are touching the LayerMask
+        bool touchingGround = playerCollider.IsTouchingLayers(groundLayerMask);
+
+        bool jumpButtonPressed = Input.GetButtonDown(jumpButton);
+        if (jumpButtonPressed == true && touchingGround == true)
+        {
+            //We have pressed jump so we should set 
+            //our upward velocity to our jump speed
+            velocity.y = jumpSpeed;
+
+            //give the velocity to the rigid body
+            physicsBody.velocity = velocity;
+        }
+
+        if (velocity.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
 }
