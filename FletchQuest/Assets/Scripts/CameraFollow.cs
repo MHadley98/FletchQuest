41ab3a3607981debf79828ed61c1b564dd7cd6cf
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2019-05-19 20:38:51 +0100

    Set base of L2 Present

---
+++ FletchQuest/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform fletcher;
+
+    void fixedUpdate()
+    {
+        transform.position = new Vector3(fletcher.position.x, fletcher.position.z);
+    }
+}
