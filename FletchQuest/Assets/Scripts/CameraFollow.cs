b8fa7bf584874df26b50e8110e0bd6e291d42c2a
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2019-05-20 16:36:06 +0100

    Edited camera L2

--- FletchQuest/Assets/Scripts/CameraFollow.cs
+++ FletchQuest/Assets/Scripts/CameraFollow.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public Transform fletcher;
+    public Transform character;
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(fletcher.position.x, fletcher.position.z);
+        transform.position = new Vector3(character.position.x, character.position.y, transform.position.z);
+  
     }
 }
