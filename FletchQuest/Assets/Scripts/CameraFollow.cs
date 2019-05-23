50a21677470f884742c949c9ef90a9e7e409443b
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2019-05-23 10:49:49 +0100

    Started writing code for clamping

--- FletchQuest/Assets/Scripts/CameraFollow.cs
+++ FletchQuest/Assets/Scripts/CameraFollow.cs
@@ -8,12 +8,32 @@ public class CameraFollow : MonoBehaviour
 
     Vector3 velocity = Vector3.zero;
 
-    public float smoothTime = .1f;
+    public float smoothTime = .15f;
+
+    //allows enabling and setting of max and min screen clamp
+    public bool YMaxEnabled = false;
+    public float YMaxValue = 0;
+
+    public bool YMinEnabled = false;
+    public float YMinValue = 0;
+
+    public bool XMaxEnabled = false;
+    public float XMaxValue = 0;
+
+    public bool XMinEnabled = false;
+    public float XMinValue = 0;
+
 
     void FixedUpdate()
     {
 
+        //sets target psoition
         Vector3 targetPos = fletcher.position;
+
+        if (YMinEnabled && YMaxEnabled)
+            targetPos.y = Mathf.Clamp(targetPos.position.y, YMinValue, YMaxValue);
+        else if (YMinEnabled)
+            targetPos.y = Mathf.Clamp(targetPos.position.y, YMinValue, targetPos.position.y);
 
         targetPos.z = transform.position.z;
 
