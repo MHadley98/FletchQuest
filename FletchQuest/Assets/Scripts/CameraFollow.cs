d9e4f1fa9de70eaa918e598d38aef32e81c58202
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2019-05-23 10:42:54 +0100

    Camera follows Fletcher

--- FletchQuest/Assets/Scripts/CameraFollow.cs
+++ FletchQuest/Assets/Scripts/CameraFollow.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
+    public Transform fletcher;
 
-    public Transform character;
+    Vector3 velocity = Vector3.zero;
+
+    public float smoothTime = .1f;
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(character.position.x, character.position.y, transform.position.z);
-  
+
+        Vector3 targetPos = fletcher.position;
+
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
     }
+    
 }
